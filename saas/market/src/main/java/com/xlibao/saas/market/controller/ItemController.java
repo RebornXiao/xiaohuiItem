e9928363a5c85016c9b13f04a38a41d509f2f4a7
chinahuangxc <chinahuangxc@vip.qq.com>
2017-07-10 18:11:34 +0800

    导入controller

--- saas/market/src/main/java/com/xlibao/saas/market/controller/ItemController.java
+++ saas/market/src/main/java/com/xlibao/saas/market/controller/ItemController.java
@@ -1,7 +1,12 @@
 package com.xlibao.saas.market.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author chinahuangxc on 2017/7/10.
  */
+@Controller
+@RequestMapping(value = "/market/item")
 public class ItemController {
 }
