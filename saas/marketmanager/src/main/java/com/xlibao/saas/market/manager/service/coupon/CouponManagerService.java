3e857db9558936581fd27430ea0edc5dce415b82
xiaohuijohn <john@xiaohuistore.com>
2017-10-17 16:30:29 +0800

    優惠卷

---
+++ saas/marketmanager/src/main/java/com/xlibao/saas/market/manager/service/coupon/CouponManagerService.java
@@ -0,0 +1,47 @@
+package com.xlibao.saas.market.manager.service.coupon;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * Created by user on 2017/10/16.
+ */
+public interface CouponManagerService {
+
+    /**
+     * 优惠券列表
+     * @return
+     */
+    JSONObject searchActivePage();
+
+    /**
+     * 优惠券使用者
+     * @return
+     */
+    JSONObject getActiveRecords();
+
+
+    /**
+     * 修改优惠券
+     * @return
+     */
+    JSONObject saveActiveRule();
+
+    /**
+     * 修改优惠券规则
+     * @return
+     */
+    JSONObject updateActiveRule();
+
+
+    /**
+     * 获取优惠券规则
+     * @return
+     */
+    JSONObject getActiveRule();
+
+    /**
+     * 删除优惠券
+     * @return
+     */
+    JSONObject delActive();
+}
